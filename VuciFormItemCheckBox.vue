04dbdd6bf9940945161df88ba8aaf720c21a787d
beniusis <rokasbeniu@gmail.com>
2023-03-01 16:29:17 +0200

    initial commit

---
+++ VuciFormItemCheckBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <vuci-form-item-template v-bind="VuciFormItemTemplateProps">
+    <a-checkbox v-model="model"/>
+  </vuci-form-item-template>
+</template>
+
+<script>
+import VuciFormItemMixin from './VuciFormItemMixin'
+
+export default {
+  name: 'VuciFormItemCheckBox',
+  mixins: [VuciFormItemMixin]
+}
+</script>
