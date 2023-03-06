9442eecce4e0fbd8f741787b7c4031fe057ebce4
beniusis <rokasbeniu@gmail.com>
2023-03-06 08:16:12 +0200

    functionality with different values

--- VuciFormItemCheckBox.vue
+++ VuciFormItemCheckBox.vue
@@ -9,6 +9,33 @@ import VuciFormItemMixin from './VuciFormItemMixin'
 
 export default {
   name: 'VuciFormItemCheckBox',
-  mixins: [VuciFormItemMixin]
+  mixins: [VuciFormItemMixin],
+  props: {
+    trueValue: {
+      type: [String, Boolean, Number],
+      default: '1'
+    },
+    falseValue: {
+      type: [String, Boolean, Number],
+      default: '0'
+    }
+  },
+  methods: {
+    convertUciValue (value) {
+      if (typeof value !== 'boolean') return value === this.trueValue
+      return value
+    },
+    __save () {
+      if (this.changed()) {
+        if (this.save) { return this.save(this) }
+
+        if (this.model) {
+          this.$uci.set(this.config, this.sid, this.name, this.trueValue)
+        } else {
+          this.$uci.set(this.config, this.sid, this.name, this.falseValue)
+        }
+      }
+    }
+  }
 }
 </script>
